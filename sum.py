c371a397249c19782dc87cff960344b41f024406
Divya Malik <divisahi1@gmail.com>
2019-02-11 09:14:16 +0000

    python-program

---
+++ sum.py
@@ -0,0 +1,4 @@
+Number1 = input("To add two numbers, enter the first number:")
+Number2 = input("Enter the second number:")
+Sum = int(Number1) + int(Number2)
+print('The sum of {0} and {1} is {2}.'.format(Number1, Number2, Sum))
